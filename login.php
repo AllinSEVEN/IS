9597abe4e62fb23d00b8ff6d37be8721aa019c99
AllinSEVEN <kaweetangkitiwongporn@gmail.com>
2026-04-22 15:52:48 +0700

    Update login.php

--- login.php
+++ login.php
@@ -44,7 +44,7 @@ if (isset($_POST['login'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>เข้าสู่ระบบ | Internship System</title>
-    <link rel="stylesheet" href="CSS/main.css">
+    <link rel="stylesheet" href="main.css">
     <style>
         body {
             display: flex;
