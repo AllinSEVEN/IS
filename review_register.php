3a3f385fba388a89c1b351d477a681ea69924f4f
AllinSEVEN <kaweetangkitiwongporn@gmail.com>
2026-04-22 15:53:46 +0700

    Update review_register.php

--- review_register.php
+++ review_register.php
@@ -68,7 +68,7 @@ $data = $_POST;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ตรวจสอบข้อมูล | Internship System</title>
-    <link rel="stylesheet" href="CSS/main.css">
+    <link rel="stylesheet" href="main.css">
 </head>
 <body>
 
